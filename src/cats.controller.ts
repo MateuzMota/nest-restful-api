f89bc7729ae255726a65d8e29513d7f7e215b6f0
Mateus Mota <m4t3u5.m0t4@gmail.com>
2024-08-21 22:06:08 -0300

    feat: create findOne method to get an especific cat by id

--- src/cats.controller.ts
+++ src/cats.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, HttpCode, Param, Post } from '@nestjs/common';
 
 @Controller('cats')
 export class CatsController {
   @Post()
+  @HttpCode(204)
   create(): string {
     return 'This action adds a new cat';
   }
@@ -10,5 +11,11 @@ export class CatsController {
   @Get()
   findAll(): string {
     return 'This action returns call cats';
+  }
+
+  @Get(':id')
+  findOne(@Param() params: any): string {
+    console.log(params.id);
+    return `This action returns a #${params.id} cat`;
   }
 }
