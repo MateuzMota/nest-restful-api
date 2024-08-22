d70881f40c35691583c842d1f89382334f28a727
Mateus Mota <m4t3u5.m0t4@gmail.com>
2024-08-21 22:31:01 -0300

    refactor: ensure that findOne use id param

--- src/cats.controller.ts
+++ src/cats.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { CreateCatDto } from './dtos/create-cat.dto';
 
 @Controller('cats')
@@ -14,8 +14,7 @@ export class CatsController {
   }
 
   @Get(':id')
-  findOne(@Param() params: any): string {
-    console.log(params.id);
-    return `This action returns a #${params.id} cat`;
+  findOne(@Param('id') id: string): string {
+    return `This action returns a #${id} cat`;
   }
 }
