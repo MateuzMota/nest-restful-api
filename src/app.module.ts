57f1d14879b1f1f8e92261ae10908684473cdf70
Mateus Mota <m4t3u5.m0t4@gmail.com>
2024-08-22 12:47:43 -0300

    fix: add CatsService to providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CatsController } from './controllers/cats.controller';
+import { CatsService } from './providers/cats.service';
 
 @Module({
   imports: [],
   controllers: [CatsController],
-  providers: [],
+  providers: [CatsService],
 })
 export class AppModule {}
